a654c8c7995d935f0038d7a114559b226aebb5c4
moba1 <moba2@protonmail.com>
2024-11-11 22:11:52 +0900

    add functionality to check arguments and generate error if there is no input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Error: required input file name");
+        process::exit(1)
+    }
+    println!("{:?}", args);
 }
