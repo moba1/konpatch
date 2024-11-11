0611925670edd3c4ce7f4fcc977e2b6bf397cb70
moba1 <moba2@protonmail.com>
2024-11-11 22:29:56 +0900

    change variable name: input_file -> source_file

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Error: required input file name");
+        eprintln!("Error: required source file path");
         process::exit(1)
     }
-    let input_file_path = &args[1];
+    let source_file_path = &args[1];
     let output_file_path = args
         .get(2)
         .map_or("a.out", |v| v.as_str());
-    let mut input_file = match fs::File::open(input_file_path) {
+    let mut source_file = match fs::File::open(source_file_path) {
         Err(why) => { eprintln!("cannot open source file: {}", why); process::exit(2) },
         Ok(file) => file,
     };
